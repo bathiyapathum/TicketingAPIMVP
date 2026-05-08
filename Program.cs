bc35dcc39193e5667e048bdaac26862eae845765
Gunaratne M H B P T <99188331+bathiyapathum@users.noreply.github.com>
2026-05-09 01:41:44 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using FlightBookingApi.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
